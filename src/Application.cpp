35b896d8a27d92b1cdb8dae4c3ddca5148558a02
mitchcamza <192mitch88@gmail.com>
2022-08-27 22:22:08 +0200

    Using Modern OpenGL in C++

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,4 +1,8 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
 
 int main(void)
 {
@@ -18,6 +22,15 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Error!" << std::endl;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -39,5 +52,5 @@ int main(void)
     }
 
     glfwTerminate();
-    return 0;
+    return 0; //
 }
